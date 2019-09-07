a70801c1280583b3fdab7cd15f852a106133c2c4
billycust <billycust@qq.com>
2019-09-07 23:18:00 +0800

    edit in dev2222

--- src/main/java/com/loczb/git/HelloWorld.java
+++ src/main/java/com/loczb/git/HelloWorld.java
@@ -6,7 +6,7 @@ package com.loczb.git;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world!2222");
         System.out.println("hello dev");
     }
 }
