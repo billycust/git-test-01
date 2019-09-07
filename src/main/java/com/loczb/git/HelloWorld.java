c1fd8e1ab9bbb8aa40431ed9a565d22782bcaaa5
billycust <billycust@qq.com>
2019-09-07 23:04:10 +0800

    first commit

---
+++ src/main/java/com/loczb/git/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.loczb.git;
+
+/**
+ * @author zhangvae
+ * @creationDate 2019/9/7
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
